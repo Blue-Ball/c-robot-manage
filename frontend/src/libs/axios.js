44fa99141c0c719ab33157f0c77c468b4be71851
Blue-Ball <ShkuratovMong@mail.ru>
2021-09-06 21:04:40 +0900

    origin axios.js

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -6,9 +6,9 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: 'https://127.0.0.1:8000/',
-  timeout: 1000,
-  headers: {'X-Custom-Header': 'foobar'}
+//   baseURL: 'https://127.0.0.1:8000/',
+//   timeout: 1000,
+//   headers: {'X-Custom-Header': 'foobar'}
 })
 
 Vue.prototype.$http = axiosIns
