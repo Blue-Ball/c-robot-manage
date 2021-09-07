fa2d000b1bd4540abbb264d24b684e7a87407d8c
Blue-Ball <ShkuratovMong@mail.ru>
2021-09-07 17:41:55 +0900

    mobile api

--- frontend/src/libs/acl/ability.js
+++ frontend/src/libs/acl/ability.js
@@ -5,7 +5,8 @@ import { initialAbility } from './config'
 // * Handles auto fetching previous abilities if already logged in user
 // ? You can update this if you store user abilities to more secure place
 // ! Anyone can update localStorage so be careful and please update this
-const userData = JSON.parse(localStorage.getItem('userData'))
-const existingAbility = userData ? userData.ability : null
+// const userData = JSON.parse(localStorage.getItem('userData'))
+// const existingAbility = userData ? userData.ability : null
+const existingAbility = null
 
 export default new Ability(existingAbility || initialAbility)
